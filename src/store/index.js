8241d4b5627c007f818a8c5af870d61b8820acd1
Molimat <matheusmolin@poli.ufrj.br>
2018-11-30 10:31:30 -0200

    Começando a configurar o SAGA

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 import { rootReducer } from './reducers/index';
+import createSagaMiddleware from 'redux-saga'
 
+import rootSaga from './sagas'
 
 const persistConfig = {
   key: 'root',
@@ -10,9 +12,13 @@ const persistConfig = {
   timeout: 0,
 }
 
+const sagaMiddleware =  createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const store = createStore(persistedReducer)
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware))
 const persistor = persistStore(store)
+
+sagaMiddleware.run(rootSaga)
 
 export {store, persistor}
